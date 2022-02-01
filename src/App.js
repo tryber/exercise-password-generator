96e15e04d9453e60a27cf8742af128e17e414ebe
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-01 14:50:58 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </main>
   );
 }
 
