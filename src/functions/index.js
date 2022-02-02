7c3010dcd047e7b5a28fd01611330648f15de129
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-02 16:14:27 -0300

    generatePassword

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,36 @@
-// export const generateWord = (wordLength, { upper, lower, hasNumbers, symbols }) => {
-//   const ALL_NUMBERS = '0123456789';
-//   const ALL_LOWER = 'abcdefghijklmnopqrstuvwxyz';
-//   const ALL_UPPER = ALL_LOWER.toUpperCase();
-//   const ALL_SYMBOLS = '!@#$%*(.)+-{><}';
+const generateWord = (wordLength, {
+  firstChecked: upper, secondChecked: lower,
+  thirdChecked: hasNumbers, fourthChecked: symbols }) => {
+  const exist = upper || lower || hasNumbers || symbols;
+  if (!exist) {
+    return 'CLICK GENERATE';
+  }
+  const ALL_NUMBERS = '0123456789';
+  const ALL_LOWER = 'abcdefghijklmnopqrstuvwxyz';
+  const ALL_UPPER = ALL_LOWER.toUpperCase();
+  const ALL_SYMBOLS = '!@#$%*(.)+-{><}';
+  let result = '';
+  let allCharacters = '';
+  if (upper) {
+    result += ALL_UPPER[Math.floor(Math.random() * ALL_UPPER.length)];
+    allCharacters += ALL_UPPER;
+  }
+  if (lower) {
+    result += ALL_LOWER[Math.floor(Math.random() * ALL_LOWER.length)];
+    allCharacters += ALL_LOWER;
+  }
+  if (hasNumbers) {
+    result += ALL_NUMBERS[Math.floor(Math.random() * ALL_NUMBERS.length)];
+    allCharacters += ALL_NUMBERS;
+  }
+  if (symbols) {
+    result += ALL_SYMBOLS[Math.floor(Math.random() * ALL_SYMBOLS.length)];
+    allCharacters += ALL_SYMBOLS;
+  }
+  while (result.length < wordLength) {
+    result += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+  return result;
+};
 
-// };
+export default generateWord;
