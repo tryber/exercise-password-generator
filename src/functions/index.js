19b9d17bb1dc2f072cc8268f78d4f0c704c29581
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-02 12:45:29 -0300

    função auxiliar sendo criada

---
+++ src/functions/index.js
@@ -0,0 +1,7 @@
+// export const generateWord = (wordLength, { upper, lower, hasNumbers, symbols }) => {
+//   const ALL_NUMBERS = '0123456789';
+//   const ALL_LOWER = 'abcdefghijklmnopqrstuvwxyz';
+//   const ALL_UPPER = ALL_LOWER.toUpperCase();
+//   const ALL_SYMBOLS = '!@#$%*(.)+-{><}';
+
+// };
