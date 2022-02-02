8b45aac9dd05cdd3bc9c9dddee3c2ecd322994f9
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-02 16:14:18 -0300

    password na tela

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import generateWord from '../functions';
 
 class Header extends React.Component {
   render() {
     const { passwordData, inputsData } = this.props;
-    // const password = generatePassword(passwordData.passwordLength, inputsData);
+    const password = generateWord(passwordData.passwordLength, inputsData);
     return (
       <header>
         <h1>Password Generator</h1>
         <section>
-          <h3>{passwordData.passwordLength || 'CLICK GENERATE'}</h3>
-          <h4>{inputsData.firstChecked ? 'é vdd' : 'not'}</h4>
+          <h3>{password}</h3>
+          {/* <h4>{inputsData.firstChecked ? 'é vdd' : 'not'}</h4>
           <h4>{inputsData.secondChecked ? 'é vdd' : 'not'}</h4>
           <h4>{inputsData.thirdChecked ? 'é vdd' : 'not'}</h4>
-          <h4>{inputsData.fourthChecked ? 'é vdd' : 'not'}</h4>
+          <h4>{inputsData.fourthChecked ? 'é vdd' : 'not'}</h4> */}
         </section>
       </header>
     )
