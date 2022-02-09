a19d6ef25c8d6e14428a07a58798bb52ba4ca02c
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-02-09 19:54:35 -0300

    testes concluidos

--- src/tests/PasswordEachOption.test.js
+++ src/tests/PasswordEachOption.test.js
@@ -5,50 +5,12 @@ import App from '../App';
 import renderWithRedux from '../helpers/renderWithRedux';
 import caracters from '../data/caracters';
 
-describe('Testa o tamanho da senha', () => {
-  it('Verifica se a senha é gerada como o tamanho de 17 caracteres', () => {
-    const length = 17;
-    renderWithRedux(<App />, { initialState: { lengthPasswordReducer: {
-      length } } });
-
-    const optionUppercase = screen.getByRole('checkbox', { name: /Include Uppercase/i });
-    userEvent.click(optionUppercase);
-    const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
-    userEvent.click(btnGenerate);
-    const password = screen.getByTestId('password');
-    expect(password.innerHTML).toHaveLength(length);
-  });
-
-  it('Verifica se a senha é gerada como o tamanho de 4 caracteres', () => {
-    const length = 4;
-    renderWithRedux(<App />);
-
-    const optionUppercase = screen.getByRole('checkbox', { name: /Include Uppercase/i });
-    userEvent.click(optionUppercase);
-    const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
-    userEvent.click(btnGenerate);
-    const password = screen.getByTestId('password');
-    expect(password.innerHTML).toHaveLength(length);
-  });
-
-  it('Verifica se a senha é gerada como o tamanho de 20 caracteres', () => {
-    const length = 20;
-    renderWithRedux(<App />, { initialState: { lengthPasswordReducer: {
-      length } } });
-
-    const optionUppercase = screen.getByRole('checkbox', { name: /Include Uppercase/i });
-    userEvent.click(optionUppercase);
-    const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
-    userEvent.click(btnGenerate);
-    const password = screen.getByTestId('password');
-    expect(password.innerHTML).toHaveLength(length);
-  });
-});
-
 describe('Verifica os caracteres da senha', () => {
-  it('Verifica se todos os caracteres são maiúsculas', () => {
+  beforeEach(() => {
     renderWithRedux(<App />);
+  });
 
+  it('Verifica se todos os caracteres são maiúsculas', () => {
     const optionUppercase = screen.getByRole('checkbox', { name: /Include Uppercase/i });
     const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
 
@@ -56,15 +18,14 @@ describe('Verifica os caracteres da senha', () => {
     userEvent.click(btnGenerate);
 
     const password = screen.getByTestId('password');
-    const composePassword = password.innerHTML.split('');
-    const validation = composePassword
+    const passwordCompose = password.innerHTML.split('');
+    const validation = passwordCompose
       .every((character) => character === character.toUpperCase());
+
     expect(validation).toBeTruthy();
   });
 
   it('Verifica se todos os caracteres são minúsculos', () => {
-    renderWithRedux(<App />);
-
     const optionLowercase = screen.getByRole('checkbox', { name: /Include Lowercase/i });
     const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
 
@@ -72,15 +33,14 @@ describe('Verifica os caracteres da senha', () => {
     userEvent.click(btnGenerate);
 
     const password = screen.getByTestId('password');
-    const composePassword = password.innerHTML.split('');
-    const validation = composePassword
+    const passwordCompose = password.innerHTML.split('');
+    const validation = passwordCompose
       .every((character) => character === character.toLowerCase());
+
     expect(validation).toBeTruthy();
   });
 
   it('Verifica se todos os caracteres são números', () => {
-    renderWithRedux(<App />);
-
     const optionNumbers = screen.getByRole('checkbox', { name: /Include Numbers/i });
     const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
 
@@ -88,15 +48,14 @@ describe('Verifica os caracteres da senha', () => {
     userEvent.click(btnGenerate);
 
     const password = screen.getByTestId('password');
-    const composePassword = password.innerHTML.split('');
-    const validation = composePassword
+    const passwordCompose = password.innerHTML.split('');
+    const validation = passwordCompose
       .every((character) => caracters.numbers.includes(character));
+
     expect(validation).toBeTruthy();
   });
 
   it('Verifica se todos os caracteres são símbolos', () => {
-    renderWithRedux(<App />);
-
     const optionSymbols = screen.getByRole('checkbox', { name: /Include Symbols/i });
     const btnGenerate = screen.getByRole('button', { name: /GENERATE PASSWORD/i });
 
@@ -104,9 +63,10 @@ describe('Verifica os caracteres da senha', () => {
     userEvent.click(btnGenerate);
 
     const password = screen.getByTestId('password');
-    const composePassword = password.innerHTML.split('');
-    const validation = composePassword
+    const passwordCompose = password.innerHTML.split('');
+    const validation = passwordCompose
       .every((character) => caracters.symbols.includes(character));
+
     expect(validation).toBeTruthy();
   });
 });
